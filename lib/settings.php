9433d0fc9411c7911e91f52a15d60f7a0b96784e
supershame <1981865127@qq.com>
2014-01-11 20:06:18 +0800

    Create settings.php

---
+++ lib/settings.php
@@ -0,0 +1,9 @@
+<?php 
+class settings{
+	public static $DEBUG = 1;
+	public static $TEMP_FILE_FOLDER = "tmp";
+	public static $IMAGE_UPLOAD_FOLDER = "uicon";
+	public static $ZIPFILE_UPLOAD_FOLDER = "zipfile";
+	public static $HOT_DAYS = 5;	
+}
+?>
